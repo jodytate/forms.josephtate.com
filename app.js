08528a4166b7dae269e0cc2f1c1ba98b0df7356a
Jody Tate <jody@josephtate.com>
2015-03-29 18:42:12 -0700

    randomize example

--- app.js
+++ app.js
@@ -3,17 +3,36 @@
 
   _.mixin(s.exports());
 
+  var randomize = function randomize (array) {
+    return array[Math.floor(Math.random() * array.length)];
+  };
+
   angular.module('wordsApp', [])
 
   .controller('WordsController', function ($scope) {
 
     $scope.words = '';
 
-    $scope.mangle = function (value) {
+    $scope.otherWords = '';
+
+    $scope.mangle = function mangle (value) {
+    
       $scope.reversed = value.split('').reverse().join('');
       $scope.uppercase = value.toUpperCase();
       $scope.echo = _.repeat(value, 3);
       $scope.spaceless = value.replace(/\s+/g, '');
+
+    };
+
+    $scope.randomMangle = function randomMangle (value) {
+    
+      var reversed = value.split('').reverse().join('');
+      var uppercase = value.toUpperCase();
+      var echo = _.repeat(value, 3);
+      var spaceless = value.replace(/\s+/g, '');
+
+      $scope.random = randomize([reversed, uppercase, echo, spaceless]);
+    
     };
 
   });
